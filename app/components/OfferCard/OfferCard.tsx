697c18867eb6591d979f5285b85dc9cc4a2a1e56
Plekson <pleksonlp@gmail.com>
2023-11-24 20:21:44 +0100

    fix

--- app/components/OfferCard/OfferCard.tsx
+++ app/components/OfferCard/OfferCard.tsx
@@ -15,7 +15,7 @@ const OfferCard = (props: Props) => {
   return (
     <div
       style={bgImage}
-      className="max-w-sm bg-cover bg-center h-[576px] w-full"
+      className="max-w-sm 2xl:w-96 bg-cover bg-center h-[576px] w-full"
     >
       <div className="flex w-full h-full items-end">
         <h3 className="text-3xl bg-black/50 text-white p-4 w-full text-center">
